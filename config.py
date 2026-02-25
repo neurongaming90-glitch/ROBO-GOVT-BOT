274c6d01a2101ced972371e7a59a2e35dbea1108
neurongaming90-glitch <neurongaming90@gmail.com>
2026-02-25 11:13:42 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8155847480:AAHiRP1qzcK27SgIaY9kdSFN5QGMxct5sX0")
-ADMIN_ID = os.environ.get("ADMIN_ID", "6593860853")
-CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", "@Roboallbotchannel")
-FETCH_INTERVAL_MINUTES = int(os.environ.get("FETCH_INTERVAL_MINUTES", "15"))
-DATABASE_PATH = os.environ.get("DATABASE_PATH", "govtjobs.db")
